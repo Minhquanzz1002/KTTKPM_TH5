49088702bae4233a4ca849c93e897f36e815b2d1
Nguyễn Minh Quân <20098151.quan@student.iuh.edu.vn>
2023-07-06 15:37:07 +0700

    init project

---
+++ src/main/java/vn/edu/iuh/SpringBootApiCourseCrudApplication.java
@@ -0,0 +1,13 @@
+package vn.edu.iuh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApiCourseCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootApiCourseCrudApplication.class, args);
+    }
+
+}
